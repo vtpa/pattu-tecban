4f08e0b4dd7fa947025c95b1ab32e0d600fb0fee
Vinicius Passos <viniciuspa87@hotmail.com>
2020-07-26 23:32:38 -0300

    Up Home Icons

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -8,6 +8,11 @@ import Balance from '../../components/Balance';
 import './styles.css';
 
 import ico1Img from '../../assets/ico1.png'
+import ico2Img from '../../assets/ico2.png'
+import ico3Img from '../../assets/ico3.png'
+import ico4Img from '../../assets/ico4.png'
+import ico5Img from '../../assets/ico5.png'
+import ico6Img from '../../assets/ico6.png'
 
 export default function Home() {
 
@@ -25,35 +30,35 @@ export default function Home() {
 
             <Link to="/map/near">
                 <div className="item-menu">
-                    <img src={ico1Img} alt="Icone Menu"/>
+                    <img src={ico2Img} alt="Icone Menu"/>
                     <h3>Estabelecimentos Próximos</h3>
                 </div>
             </Link>
 
             <Link to="/map/atms">
                 <div className="item-menu">
-                    <img src={ico1Img} alt="Icone Menu"/>
+                    <img src={ico3Img} alt="Icone Menu"/>
                     <h3>Caixas Eletrônicos</h3>
                 </div>
             </Link>
 
             <Link to="/map/agencies">
                 <div className="item-menu">
-                    <img src={ico1Img} alt="Icone Menu"/>
+                    <img src={ico4Img} alt="Icone Menu"/>
                     <h3>Agências bancárias</h3>
                 </div>
             </Link>
 
             <Link to="history">
                 <div className="item-menu">
-                    <img src={ico1Img} alt="Icone Menu"/>
+                    <img src={ico5Img} alt="Icone Menu"/>
                     <h3>Histórico de consumo</h3>
                 </div>
             </Link>
 
             <Link to="/withdraw">
                 <div className="item-menu">
-                    <img src={ico1Img} alt="Icone Menu"/>
+                    <img src={ico6Img} alt="Icone Menu"/>
                     <h3>Ordem de Pagamento Saque Digital</h3>
                 </div>
             </Link>
